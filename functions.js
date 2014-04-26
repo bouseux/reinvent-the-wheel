6b4b8b25680351a5614964f0563ed1fbc3707c3f
katzhang <arcadychang@gmail.com>
2014-04-25 21:05:01 -0400

    added bindAll and bind

--- functions.js
+++ functions.js
@@ -5,3 +5,20 @@ function bind(func, object, parameters) {
 	// var args = parameters ? Array.prototype.slice.call(parameters) : [];
 	func.call(object, parameters);
 }
+
+//bindAll(object, *methodNames)
+//Binds a number of methods on the object, specified by methodNames, 
+//to be run in the context of that object whenever they are invoked. 
+function bindAll(obj) {
+	var methods = Array.prototype.slice.call(arguments, 1);
+	methods.forEach(function(method) {
+		if(typeof method !== 'function') {
+			return;
+		} else {
+			bind(method, obj);
+		}
+	})
+
+
+
+}
